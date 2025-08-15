418503338205061fa6a31cb031607d5093733e6d
amir <amirtaki623@gmail.com>
2025-08-15 22:25:29 +0330

    js part 3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 const inputs = document.querySelectorAll('input')
+const icons = document.querySelectorAll("i")
 
 inputs.forEach((input) => {
     input.addEventListener("click", (e)=> {
-        
+
     })
 })
