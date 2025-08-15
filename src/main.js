e0578ade0a93c1c208ac4f17f86904057aa14436
amir <amirtaki623@gmail.com>
2025-08-15 22:26:47 +0330

    js part 4

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 const inputs = document.querySelectorAll('input')
 const icons = document.querySelectorAll("i")
 
-inputs.forEach((input) => {
+inputs.forEach((input, index) => {
     input.addEventListener("click", (e)=> {
-
+        icons[index].style.left = "calc(100% - 12.5px)"
     })
 })
