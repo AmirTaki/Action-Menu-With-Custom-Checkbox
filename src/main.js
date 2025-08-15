6acf850daa3ef151021335c032e030d7ec788baa
amir <amirtaki623@gmail.com>
2025-08-15 22:57:40 +0330

    complete project

--- src/main.js
+++ src/main.js
@@ -1,17 +1,6 @@
-
-
 let body = document.querySelector('body')
 let dark = document.getElementById("darkMode")
 
-
-const checkMode = () => {
-    if (dark.checked == true){
-        body.classList.add("darkMode")
-    }
-    else {
-        body.classList.remove("darkMode")
-    }
-}
 dark.addEventListener("click", () => {
-     checkMode ()
+    body.classList.toggle(dark.checked == true  ? "darkMode":"darkMode")
 })
