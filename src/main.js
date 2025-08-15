5676b4c3cff90f21e33bcde2c6067e72ad9f85ee
amir <amirtaki623@gmail.com>
2025-08-15 22:49:12 +0330

    styles part 20

--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
-// const inputs = document.querySelectorAll('.action')
-// const icons = document.querySelectorAll("i")
-
-// inputs.forEach((input, index) => {
-//     input.addEventListener("click", (e)=> {
-//         icons[index].insertAdjacentHTML = "calc(100% - 12.5px)"
-//     })
-// })
 
 
 let body = document.querySelector('body')
